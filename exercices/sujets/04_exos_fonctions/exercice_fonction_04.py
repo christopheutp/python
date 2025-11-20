adadc6329de375c259b9e60e9c5c414546b1b8ec
Christophe D. <christophe.delory@labom2iformation.fr>
2025-11-20 16:53:39 +0100

    corrections exercices

--- exercices/sujets/04_exos_fonctions/exercice_fonction_04.py
+++ exercices/sujets/04_exos_fonctions/exercice_fonction_04.py
@@ -8,3 +8,41 @@
 # Exemple : compter_lettre_a("mixer") # résultat : 0
 #
 # Écrire une autre fonction sans boucle qui utilisera count à la place.
+
+
+# Correction 1
+print("Solution 1 :")
+print() # saut de ligne 
+
+def compter_lettre_a(chaine):
+    """Compte le nombre de lettres 'a' dans la chaîne en utilisant une boucle."""
+    compteur = 0
+    for lettre in chaine:
+        if lettre == "a":
+            compteur += 1
+    return compteur
+
+# Tests de la fonction
+print(compter_lettre_a("abba"))   # Résultat : 2
+print(compter_lettre_a("mixer"))  # Résultat : 0
+
+# Correction 2
+print() # saut de ligne 
+print("Solution 2 :")
+print() # saut de ligne 
+
+def compter_lettre_a_count(chaine):
+    """Compte le nombre de lettres 'a' dans la chaîne en utilisant count()."""
+    return chaine.count("a")
+
+# Tests de la fonction
+print(compter_lettre_a_count("abba"))   # Résultat : 2
+print(compter_lettre_a_count("mixer"))  # Résultat : 0
+
+
+def compter_lettre_x_count(chaine, lettre = "a"):
+    """Compte le nombre de lettres 'a' dans la chaîne en utilisant count()."""
+    return chaine.count(lettre)
+
+print(compter_lettre_x_count("abba"))   # Résultat : 2
+print(compter_lettre_x_count("mixer","e"))  # Résultat : 0
